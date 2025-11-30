d1a22275d8847b9ab7d4ae1b5369d5dfcba07037
Lukáš Heisig <luk.heisig@gmail.com>
2025-11-30 07:38:49 +0000

    File changes

--- src/pages/MobileHome.jsx
+++ src/pages/MobileHome.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { useOffline } from "@/components/OfflineProvider";
 import { format } from "date-fns";
