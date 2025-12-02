40966766d68c0fc78c9b3e543e834fff394dfa5c
Lukáš Heisig <luk.heisig@gmail.com>
2025-12-02 09:12:48 +0000

    File changes

--- src/components/TranslationContext.jsx
+++ src/components/TranslationContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { translations } from '@/utils/translations';
+import { translations } from '@/components/translations';
 
 const TranslationContext = createContext();
 
