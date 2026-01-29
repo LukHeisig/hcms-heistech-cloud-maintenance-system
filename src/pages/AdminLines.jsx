c73a7a0ca773dba1b94805fbacfdd02f15dfb22b
Lukáš Heisig <luk.heisig@gmail.com>
2026-01-29 23:00:24 +0000

    File changes

--- src/pages/AdminLines.jsx
+++ src/pages/AdminLines.jsx
@@ -104,7 +104,10 @@ export default function AdminLines() {
 
   const { data: allUsers = [] } = useQuery({
     queryKey: ["allUsers"],
-    queryFn: () => base44.entities.User.list(),
+    queryFn: async () => {
+      const { data } = await base44.functions.invoke("getUsers");
+      return data;
+    },
   });
 
   const companyUsers = React.useMemo(() => {
